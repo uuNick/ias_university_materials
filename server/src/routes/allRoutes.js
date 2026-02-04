31d7e32cf5507cf35908be4b8201cd576d29959f
uuNick <nikitasidarenko@gmail.com>
2026-02-04 11:47:02 +0300

    Add server.js file, routes folder, and empty db.js file

---
+++ server/src/routes/allRoutes.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+
+//const AuthRouter = require("./AuthRouter");
+
+//router.use("/auth", AuthRouter);
+module.exports = router;
---
+++ server/src/server.js
@@ -0,0 +1,39 @@
+require("dotenv").config();
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+//const sequelize = require("./db");
+//const models = require("./models/Models");
+const router = require("./routes/allRoutes");
+//const { EmployeeSeed, DocumentSeed, SickLeaveSeed, VacationSeed, DayOffSeed, UserSeed, RoleSeed, UserRoleSeed } = require("./seed");
+
+const port = process.env.SERVER_PORT;
+const app = express()
+
+const corsOptions = {
+    origin: process.env.DOMAIN,
+    credentials: true,
+    optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
+
+app.use(express.json());
+//app.use(express.static(path.join(__dirname, 'uploads')));
+app.use("/api", router);
+app.get("/", function(request, response){
+     
+    // отправляем ответ
+    response.send("<h2>Привет Express!</h2>");
+});
+
+const start = async () => {
+    try {
+        //await sequelize.authenticate();
+        app.listen(port, () => console.log(`Server listening on port ${port}`));
+    } catch (e) {
+        console.log(e);
+    }
+};
+
+start();
